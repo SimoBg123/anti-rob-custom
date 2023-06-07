73e88033cfc49d4ac155bfe3b763d4b0a3587cc6
SimoBg123 <98186395+SimoBg123@users.noreply.github.com>
2023-06-07 22:23:51 +0300

    Update fxmanifest.lua

--- anti-rob/fxmanifest.lua
+++ anti-rob/fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Your Name'
-description 'Anti-Rob Command'
+author 'Simeoncho#7008'
+description 'Anti-Rob Command by Simeoncho FOR QBCore'
 version '1.0.0'
 
 client_scripts {
